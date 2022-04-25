0e86c9e2f5d56da69c7015534b85c230cee485a2
M1TE5H <M1TE5H@oqtopi.com>
2022-04-25 18:13:46 +0100

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -7,7 +7,7 @@ void generate () {
   indexfile << "<!DOCTYPE html>\n";
   indexfile << "<html>\n";
   indexfile << "\t<head>\n";
-  indexfile << "\t\t<title>flohrere</title>\n";
+  indexfile << "\t\t<title> culture and cuisine | flohrere - berlin blogger </title>\n";
   indexfile << "\t</head>\n";
   indexfile << "\t<body>\n";
   indexfile << "\t\t<h1>flohrere</h1>\n";
